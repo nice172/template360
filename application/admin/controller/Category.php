4b7ee60e5ecd1a596d489d0c3e2a9d93a4259ebe
liaoyinzong <354575573@qq.com>
2019-02-21 18:05:30 +0800

    no message

---
+++ application/admin/controller/Category.php
@@ -0,0 +1,33 @@
+<?php
+namespace app\admin\controller;
+class Category extends Base {
+    
+    public function index(){
+        
+    }
+    
+    public function add(){
+        
+    }
+    
+    public function addpost(){
+        
+    }
+    
+    public function edit(){
+        
+    }
+    
+    public function editpost(){
+        
+    }
+    
+    public function delete(){
+        
+    }
+    
+    public function listsort(){
+        
+    }
+    
+}
